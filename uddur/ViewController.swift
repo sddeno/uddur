d4a38c30e74d41b06174384aad4cd077637970ee
shubhamdeshmukh69 <shubhamdeshmukh69@gmail.com>
2020-05-06 15:09:27 +0530

    label

---
+++ uddur/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uddur
+//
+//  Created by shubham on 06/05/20.
+//  Copyright © 2020 Shubham Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
